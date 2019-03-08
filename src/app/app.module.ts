a66e95c6f21da8757dbe0069a7028b097f958b75
Jyotshna Pedabaliyarasimhuni <Jyotshna.Pedabaliyarasimhuni@ey.com>
2019-03-08 13:47:00 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { TasklogsheetComponent } from './tasklogsheet/tasklogsheet.component';
-import { NgxWidgetGridModule } from 'ngx-widget-grid';
 import { TaskListComponent } from './task-list/task-list.component';
 // import { HeroComponent } from './task-list/Hero';
+// import { HttpModule } from '@angular/http';
+// import { NgxWidgetGridModule } from 'ngx-widget-grid';
 
 @NgModule({
   declarations: [
     AppComponent,
     TasklogsheetComponent,
-    TaskListComponent//,
+    TaskListComponent
     // HeroComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    NgxWidgetGridModule
+    // HttpModule,
+    // NgxWidgetGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
