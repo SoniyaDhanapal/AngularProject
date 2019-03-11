b6cc1d3a3e6f7e88de6d5b282c0d0f779eda4243
Jyotshna Pedabaliyarasimhuni <Jyotshna.Pedabaliyarasimhuni@ey.com>
2019-03-11 18:47:46 +0530

    commit

--- src/app/module/Hero.ts
+++ src/app/module/Hero.ts
@@ -2,14 +2,14 @@ export class HeroModel {
   id: number;
   name: string;
 }
-export const Developers: HeroModel[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 16, name: 'RubberMan' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+// export const Developers: HeroModel[] = [
+//   { id: 11, name: 'Mr. Nice' },
+//   { id: 12, name: 'Narco' },
+//   { id: 16, name: 'RubberMan' },
+//   { id: 18, name: 'Dr IQ' },
+//   { id: 19, name: 'Magma' },
+//   { id: 20, name: 'Tornado' }
+// ];
 
 const json_text = ` [
   {
@@ -21,4 +21,6 @@ const json_text = ` [
     "name": "Pablo Escobar"
   }
 ]`;
+// converting to Object
+
 export const test: HeroModel[] = JSON.parse(json_text);
